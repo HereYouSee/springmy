b7b736600dd15301c7f937522c28cedfa10af9c8
HereYousee <935030965@qq.com>
2018-06-02 21:42:26 +0800

    111

--- src/main/java/MainBootStrap.java
+++ src/main/java/MainBootStrap.java
@@ -5,7 +5,9 @@ public class MainBootStrap {
 
     public static void main(String[] args) {
 
+        System.out.println("this is the master");
 
+        System.out.println("now i am i dev1");
 
     }
 }
